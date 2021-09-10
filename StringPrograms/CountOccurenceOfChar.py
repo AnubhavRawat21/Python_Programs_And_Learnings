762c968ca7c2e13971aaf71b00b2bd4f9502a366
Sumit Sharma <sumitaccess007@gmail.com>
2021-09-10 21:12:42 +0530

    Count Char Occurence and Reverse a String Program

--- StringPrograms/CountOccurenceOfChar.py
+++ StringPrograms/CountOccurenceOfChar.py
@@ -1,4 +1,36 @@
 # Program to count the number of times a character occurs in a String
 # Accept String as Input From User
 # Accept Character to count as Input From User
-# Print Occurence of that character as Output
+# Print Occurence of that character as Output.
+
+# Function Definition
+def charOccurenceCount(stringParameter):
+    count=0
+    for index in range (0,len(stringParameter)):
+        char=stringParameter[index]
+        if(char==searchChar):
+            count+=1
+    return count
+
+# Input From User
+inputString=input('Enter String - ')
+searchChar=input("Enter Char To Count - ")
+
+# Method-1 : Char Count Without Function Call
+print("Char Count Without Function Call -")
+count=0
+for index in range (0,len(inputString)):
+	char=inputString[index]
+	if(char==searchChar):
+		count+=1
+print("Char", searchChar,"occurs ",count," times in String - '",inputString,"'")
+print("Bye")
+print("")
+
+
+# Method-2 : Char Count Without Function Call
+print("Char Count Thru Function Call -")
+countThruFunction=charOccurenceCount(inputString)
+print("Char", searchChar,"occurs ",countThruFunction," times in String - '",inputString,"'")
+print("Bye Again")
+print("")
--- StringPrograms/ReverseString.py
+++ StringPrograms/ReverseString.py
@@ -3,3 +3,18 @@
 # Output - Print Reversed String
 # Example - Input - str1='Hello'
 # Output - str2='olleH'
+
+def reverseString(inputString):
+    stringChar=""
+    for index in range (-1,-len(inputString)-1,-1):
+        stringChar1=inputString[index]
+        stringChar+=stringChar1
+    string2=stringChar    
+    return string2
+
+
+inputString=input('Enter String That Is To Be Reversed:-')
+print('String In Reverse Order Using Function :-')
+reversedString=reverseString(inputString)
+print('Reverse of ',inputString,'is',reversedString)
+
