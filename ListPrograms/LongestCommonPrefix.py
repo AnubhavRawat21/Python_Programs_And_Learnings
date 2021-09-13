3814b56d10d923e882cc536ff099c721b82545d1
Sumit Sharma <sumitaccess007@gmail.com>
2021-09-13 20:36:09 +0530

    List Programs

---
+++ ListPrograms/LongestCommonPrefix.py
@@ -0,0 +1,4 @@
+# Find Longest Common Prefix among the Strings stored in List Data Structured
+# Input  : [“Mine”, “Mineral”, “Mint”]
+# Output : "Min"
+# In case nothing is common return a "" empty string
